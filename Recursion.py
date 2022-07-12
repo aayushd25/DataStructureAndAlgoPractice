dab40bc3a87f0cdedb8ec349098f474c97b03414
aayushd25 <aayushboise@gmail.com>
2022-07-11 19:06:30 -0500

    updated recursion

--- Recursion.py
+++ Recursion.py
@@ -1,7 +1,7 @@
 def factorial(n):
-    if n == 0 or n == 1:
+    if n == 0 or n == 1: #base case
         return 1
-    if ( not(n < 0) ) and ( n % 10 == 0):
+    if ( not(n < 0) ) and (int(n) == n) : #tests that n is positive and is integer
         return n * factorial(n-1)
     else:
         return "Not valid"
