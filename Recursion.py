4ae66bed8a493ad15508f81faba63abf6cf399a4
aayushd25 <aayushboise@gmail.com>
2022-07-11 19:03:34 -0500

    updated recursion

--- Recursion.py
+++ Recursion.py
@@ -1,5 +1,8 @@
 def factorial(n):
     if n == 0 or n == 1:
         return 1
-    else:
+    if ( not(n < 0) ) and ( n % 10 == 0):
         return n * factorial(n-1)
+    else:
+        return "Not valid"
+print(factorial(10.6))
