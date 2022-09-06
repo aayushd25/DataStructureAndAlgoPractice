43c481625a3c0a9d90acb729da1be565c4f75382
aayushd25 <aayushboise@gmail.com>
2022-09-05 19:39:35 -0500

    array practice in java

---
+++ arrays.java
@@ -0,0 +1,11 @@
+public class Array {
+    public static void main(String[] args){
+        int [] arr; 
+        arr = new int[3]
+        arr[1] = 10; 
+        arr[2] = 20;
+        arr[3] = 40;
+        for (int i = 0; i < arr.length; i++)
+            System.out.println("Element at index " + i + " : " + arr[i]);
+    }
+}
